aed59f280524477bd3edc0eb323d71b42e099157
zzx <835781192@qq.com>
2023-12-28 17:29:06 +0800

    2023-12-28:captcha模块

---
+++ zzx-boot-framework/zzx-boot-starter-captcha/src/main/java/org/example/framework/captcha/config/YudaoCaptchaConfiguration.java
@@ -0,0 +1,28 @@
+package org.example.framework.captcha.config;
+
+import com.xingyuv.captcha.properties.AjCaptchaProperties;
+import com.xingyuv.captcha.service.CaptchaCacheService;
+import com.xingyuv.captcha.service.impl.CaptchaServiceFactory;
+import jakarta.annotation.Resource;
+import org.example.framework.captcha.core.service.RedisCaptchaServiceImpl;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@AutoConfiguration
+public class YudaoCaptchaConfiguration {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Bean
+    public CaptchaCacheService captchaCacheService(AjCaptchaProperties config) {
+        // 缓存类型 redis/local/....
+        CaptchaCacheService ret = CaptchaServiceFactory.getCache(config.getCacheType().name());
+        if (ret instanceof RedisCaptchaServiceImpl) {
+            ((RedisCaptchaServiceImpl) ret).setStringRedisTemplate(stringRedisTemplate);
+        }
+        return ret;
+    }
+
+}
