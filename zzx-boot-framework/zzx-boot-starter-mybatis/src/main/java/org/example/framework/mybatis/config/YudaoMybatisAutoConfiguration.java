e173800241b23c5f42e3dbed2e6863bf3834303d
zzx <835781192@qq.com>
2023-12-28 17:07:49 +0800

    2023-12-28:mybatis模块，web模块，websocket模块，test模块；

---
+++ zzx-boot-framework/zzx-boot-starter-mybatis/src/main/java/org/example/framework/mybatis/config/YudaoMybatisAutoConfiguration.java
@@ -0,0 +1,59 @@
+package org.example.framework.mybatis.config;
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.core.incrementer.IKeyGenerator;
+import com.baomidou.mybatisplus.extension.incrementer.*;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.annotations.Mapper;
+import org.example.framework.mybatis.core.handler.DefaultDBFieldHandler;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * MyBaits 配置类
+ */
+@AutoConfiguration
+// todo:
+//  value = "${yudao.info.base-package}" 是自定义的配置，需要在配置文件中配置
+@MapperScan(value = "${yudao.info.base-package}", annotationClass = Mapper.class,
+        lazyInitialization = "${mybatis.lazy-initialization:false}") // Mapper 懒加载，目前仅用于单元测试
+public class YudaoMybatisAutoConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor()); // 分页插件
+        return mybatisPlusInterceptor;
+    }
+
+    @Bean
+    public MetaObjectHandler defaultMetaObjectHandler() {
+        return new DefaultDBFieldHandler(); // 自动填充参数类
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "mybatis-plus.global-config.db-config", name = "id-type", havingValue = "INPUT")
+    public IKeyGenerator keyGenerator(ConfigurableEnvironment environment) {
+        DbType dbType = IdTypeEnvironmentPostProcessor.getDbType(environment);
+        if (dbType != null) {
+            return switch (dbType) {
+                case POSTGRE_SQL -> new PostgreKeyGenerator();
+                case ORACLE, ORACLE_12C -> new OracleKeyGenerator();
+                case H2 -> new H2KeyGenerator();
+                case KINGBASE_ES -> new KingbaseKeyGenerator();
+                case DM -> new DmKeyGenerator();
+                default ->
+                        throw new IllegalArgumentException(StrUtil.format("DbType{} 找不到合适的 IKeyGenerator 实现类", dbType));
+            };
+        }
+        // 找不到合适的 IKeyGenerator 实现类
+        throw new IllegalArgumentException(StrUtil.format("DbType{} 找不到合适的 IKeyGenerator 实现类", dbType));
+    }
+
+}
--- zzx-boot-framework/zzx-boot-starter-test/src/main/java/org/example/framework/test/core/ut/package-info.java
+++
@@ -1,1 +0,0 @@
-package org.example.framework.test.core.ut;
