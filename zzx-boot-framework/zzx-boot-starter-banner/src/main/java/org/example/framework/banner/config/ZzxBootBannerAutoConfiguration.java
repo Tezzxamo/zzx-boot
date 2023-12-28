81d24b9025efe734bbfae0e1ae3027795bda3d56
zzx <835781192@qq.com>
2023-12-28 16:22:20 +0800

    2023-12-28:banner模块补充；

--- zzx-boot-framework/zzx-boot-starter-banner/src/main/java/org/example/framework/banner/config/ZzxBootBannerAutoConfiguration.java
+++ zzx-boot-framework/zzx-boot-starter-banner/src/main/java/org/example/framework/banner/config/ZzxBootBannerAutoConfiguration.java
@@ -1,12 +1,17 @@
 package org.example.framework.banner.config;
 
 
+import org.example.framework.banner.core.BannerApplicationRunner;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
 public class ZzxBootBannerAutoConfiguration {
 
-
+    @Bean
+    public BannerApplicationRunner bannerApplicationRunner() {
+        return new BannerApplicationRunner();
+    }
 
 
 }
